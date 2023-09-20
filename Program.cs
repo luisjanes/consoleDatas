076865fbf4c9cb531bffc263319fd968ed60d32f
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-09-20 17:24:58 -0300

    feat: new date mocking values and now/utcnow

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@
             Console.WriteLine(data);
             data = DateTime.Now;
             Console.WriteLine(data);
+            data = DateTime.UtcNow;
+            Console.WriteLine(data);
+
+            data = new DateTime(1995, 03, 23, 16, 45, 00);
+            Console.WriteLine(data);
         }
     }
 }
