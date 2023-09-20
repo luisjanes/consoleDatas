940e9fad08e7aea9638b98e73407148a3e3b623b
Luis Felipe da Costa Janes <luisf.janes@gmail.com>
2023-09-20 17:15:43 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace consoleDatas
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var data = new DateTime();
+            Console.WriteLine(data);
+            data = DateTime.Now;
+            Console.WriteLine(data);
+        }
+    }
+}
